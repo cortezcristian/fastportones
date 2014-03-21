2264848899c9a207c0eb6ea016ff21481181204b
Cristian Cortez <cortez.cristian@gmail.com>
2014-03-21 01:58:49 -0300

    Adding styles to the titles

--- themes/twentythirteen/content.php
+++ themes/twentythirteen/content.php
@@ -19,10 +19,10 @@
 		<?php endif; ?>
 
 		<?php if ( is_single() ) : ?>
-		<h1 class="entry-title"><?php the_title(); ?></h1>
+		<h1 class="entry-title"><span class="fast-entry-title">Fast</span> | <?php the_title(); ?></h1>
 		<?php else : ?>
 		<h1 class="entry-title">
-			<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
+			<a href="<?php the_permalink(); ?>" rel="bookmark"><span class="fast-entry-title">Fast</span> | <?php the_title(); ?></a>
 		</h1>
 		<?php endif; // is_single() ?>
 
