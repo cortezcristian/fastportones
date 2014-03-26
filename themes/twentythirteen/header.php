aba9db9fa50d0857b01759614c3ffb0a0ee6c59f
rominadg <romina.rota@gmail.com>
2014-03-26 19:53:05 -0300

    cambios de img
    pixelada

--- themes/twentythirteen/header.php
+++ themes/twentythirteen/header.php
@@ -33,7 +33,8 @@
 <body <?php body_class(); ?>>
 	<div id="page" class="hfeed site">
 		<header id="masthead" class="site-header" role="banner">
-			<a class="home-link logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+			<a class="home-link" id="logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+				<img src="http://fastportones.com.ar/wp-content/themes/twentythirteen/images/fast-logo.png" alt="Fast Portones" height="91" width="341">
 				<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
 				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 			</a>
